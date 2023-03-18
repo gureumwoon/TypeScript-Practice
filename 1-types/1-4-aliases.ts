9816e1a92abad6e2ffe2a1bbc0b22ca13fad401a
gureumwoon <vfrvfr3221@gmail.com>
2023-03-18 16:00:41 +0900

    Type Alias

---
+++ 1-types/1-4-aliases.ts
@@ -0,0 +1,25 @@
+{
+    // Type Alisaes 새로운 타입을 내가 직접 정의할 수 있음.
+
+    type Text = string;
+    const name: Text = 'woon';
+    const address: Text = 'korea'
+    type Num = number;
+    type Student = {
+        name: string,
+        age: number;
+    };
+    const student: Student = {
+        name: 'woon',
+        age: 22
+    };
+
+    // Stirng Literal Types
+
+    type Name = 'name';
+    let woonyName: Name;
+    // woonyName ='d' 다른 건 할당 안 됨. 에러남 오직 'nqme'만 할당 가능.
+    woonyName = 'name'
+    type JSON = 'json'
+    const json: JSON = 'json'
+}
