94c52422af8235974bb2c11ee253cfb2bf88dff8
gureumwoon <vfrvfr3221@gmail.com>
2023-03-20 18:02:02 +0900

    TS 사용해서 로딩 상태 표시하기

--- 2_types_projects/2-types-projects-quiz/loading.ts
+++ 2_types_projects/2-types-projects-quiz/loading.ts
@@ -20,6 +20,22 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case 'loading':
+        console.log('loading...');
+        break;
+      case 'success':
+        console.log(`😁 ${state.response.body}`)
+        break;
+      case 'fail':
+        console.log(`😱 ${state.reason}`)
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`)
+    }
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
