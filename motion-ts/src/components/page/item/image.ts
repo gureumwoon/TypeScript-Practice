98b7b62031697b9a4884e0fbae334447c29fe103
gureumwoon <vfrvfr3221@gmail.com>
2023-04-19 20:15:28 +0900

    Image Component 완성

---
+++ motion-ts/src/components/page/item/image.ts
@@ -0,0 +1,21 @@
+export class ImageComponent {
+    private element: HTMLElement;
+    constructor(title: string, url: string) {
+        const template = document.createElement('template');
+        template.innerHTML = `<section class="image">
+        <div class="image-holder"><img class="image-thumbnail"></div>
+        <p class="image-title"></p>
+    </section>`;
+        this.element = template.content.firstElementChild! as HTMLElement;
+
+        const imageElement = this.element.querySelector('image-thumbnail')! as HTMLImageElement;
+        imageElement.src = url;
+        imageElement.alt = title;
+
+        const titleElement = this.element.querySelector('image-title')! as HTMLParagraphElement;
+        titleElement.textContent = title;
+    }
+    attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
+        parent.insertAdjacentElement(position, this.element)
+    }
+}
