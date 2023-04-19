d92f290f0d398d38cf5a50c29566ed3484e790a4
gureumwoon <vfrvfr3221@gmail.com>
2023-04-20 00:23:01 +0900

    BaseComponent ImageComponent에 상속 완료

--- motion-ts/src/components/page/item/image.ts
+++ motion-ts/src/components/page/item/image.ts
@@ -1,12 +1,11 @@
-export class ImageComponent {
-    private element: HTMLElement;
+import { BaseComponent } from "../../component.js";
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
     constructor(title: string, url: string) {
-        const template = document.createElement('template');
-        template.innerHTML = `<section class="image">
-        <div class="image-holder"><img class="image-thumbnail"></div>
-        <p class="image-title"></p>
-    </section>`;
-        this.element = template.content.firstElementChild! as HTMLElement;
+        super(`<section class="image">
+                  <div class="image-holder"><img class="image-thumbnail"></div>
+                   <p class="image-title"></p>
+                </section>`);
 
         const imageElement = this.element.querySelector('.image-thumbnail')! as HTMLImageElement;
         imageElement.src = url;
@@ -14,8 +13,5 @@ export class ImageComponent {
 
         const titleElement = this.element.querySelector('.image-title')! as HTMLParagraphElement;
         titleElement.textContent = title;
-    }
-    attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-        parent.insertAdjacentElement(position, this.element)
     }
 }
